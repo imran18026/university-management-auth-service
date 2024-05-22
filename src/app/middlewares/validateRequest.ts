5960c074e3896fe7d2071230cc51d60a51d612be
Md Imran Hossain <mdimranhossain.ist@gmail.com>
2024-05-22 13:38:10 +0600

    middlewares added successfully

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { AnyZodObject, ZodEffects } from 'zod'
 
+// validate request
 const validateRequest =
   (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
